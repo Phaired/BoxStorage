c986391b6e660e3fc42df9ca89cb7dc5fe48d69d
Phaired <remybarranco@orange.fr>
2022-12-06 09:52:05 +0100

    Initial commit

---
+++ history.sql
@@ -0,0 +1,48 @@
+/*
+This file is used to keep an history of ALL sql modification
+faut creer une table projet avant en se connectant au 127.0.0.1:55432 en utilisant root:password
+*/
+use projet;
+
+create table categories
+(
+    productCategory int auto_increment
+        primary key,
+    catName         varchar(255) not null
+);
+
+create table articles
+(
+    shoeId               varchar(128) not null
+        primary key,
+    brand                varchar(128) not null,
+    category             varchar(128) not null,
+    colorway             varchar(255) null,
+    `condition`          varchar(128) null,
+    countryOfManufacture varchar(128) null,
+    description          text         null,
+    gender               varchar(128) null,
+    imageUrl             text         null,
+    name                 varchar(128) null,
+    productCategory      int          not null,
+    releaseDate          date         null,
+    retailPrice          int          null,
+    shoe                 varchar(128) null,
+    shortDescription     text         null,
+    title                varchar(255) null,
+    tags                 varchar(255) null,
+    constraint articles_categories_null_fk
+        foreign key (productCategory) references categories (productCategory)
+);
+
+create table stocks
+(
+    id       int auto_increment
+        primary key,
+    shoeId   varchar(128) null,
+    shoeSize int          null,
+    quantity int          null,
+    constraint stocks_articles_null_fk
+        foreign key (shoeId) references articles (shoeId)
+);
+
