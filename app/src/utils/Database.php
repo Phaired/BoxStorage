15b1b7433b7bf56537203b9db53d74fbd63bbe55
Phaired <65019388+Phaired@users.noreply.github.com>
2023-02-07 15:36:08 +0100

    rework database connection

---
+++ app/src/utils/Database.php
@@ -0,0 +1,22 @@
+<?php
+class Database
+{
+    public var $db;
+
+    public function __construct()
+    {
+        $dsn="mysql:dbname=projet;host=database";
+        try
+        {
+            $this->db=new PDO($dsn,"root","password");
+        }
+        catch(PDOException $e)
+        {
+            printf("Echec connexion : %s\n",
+                $e->getMessage());
+            exit();
+        }
+
+    }
+
+}
