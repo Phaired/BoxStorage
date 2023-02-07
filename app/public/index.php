f1c5d554db6e0e70246819b285d44f339136feb6
Phaired <65019388+Phaired@users.noreply.github.com>
2023-02-07 15:23:11 +0100

    added whitelist on index and inscription pages

--- app/public/index.php
+++ app/public/index.php
@@ -1,19 +1,25 @@
 <?php
 $request = $_SERVER['REQUEST_URI'];
-
-switch ($request)
-{
-    case '/' :
-        require '../src/views/home.php';
-        break;
-    case '/login' :
-        require '../src/views/login.php';
-        break;
-    case "/catalog" :
-        require "../src/views/catalog.php";
-        break;
-    default:
-        http_response_code(404);
-        require '../src/views/404.php';
-        break;
+$authorized_pages = ["/", "/login", "/catalog"];
+if(in_array($request, $authorized_pages)) {
+    switch ($request)
+    {
+        case '/' :
+            require '../src/views/home.php';
+            break;
+        case '/login' :
+            require '../src/views/login.php';
+            break;
+        case "/catalog" :
+            require "../src/views/catalog.php";
+            break;
+        default:
+            http_response_code(404);
+            require '../src/views/404.php';
+            break;
+    }
+}
+else {
+    http_response_code(404);
+    require '../src/views/404.php';
 }
---
+++ app/src/views/inscription.php
@@ -0,0 +1,58 @@
+<?php
+echo"
+<div id='background'>
+      <div id='container'>
+        <form id='formSignUp' action=''>
+          <div class='formItems'>
+            <label for='username'>Username</label>
+            <input id='username' name='username' type='text' />
+          </div>
+
+          <div class='formItems'>
+            <label for='firstname'>FirstName</label>
+            <input id='firstname' name='firstname' type='text' />
+          </div>
+          <div class='formItems'>
+            <label for='lastname'>LastName</label>
+            <input id='lastname' name='lastname' type='text' />
+          </div>
+          <div class='formItems'>
+            <label for='email'>Email</label>
+            <input id='email' name='email' type='email' />
+          </div>
+          <div class='formItems'>
+            <label for='password'>Password</label>
+            <input id='password' name='password' type='password' />
+          </div>
+          <div class='formItems'>
+            <label for='confirmpassword'>Confirm Password</label>
+            <input
+              id='confirmpassword'
+              name='confirmpassword'
+              type='password'
+                  />
+          </div>
+          <div class='formItems' id='zipcity'>
+            <div id='zipGroup'>
+              <label for='zipcode'>Zip Code</label>
+              <input id='zipcode' name='zipcode' type='text' />
+            </div>
+            <div id='cityGroup'>
+              <label for='city'>City</label>
+              <input id='city' name='city' type='text' />
+            </div>
+          </div>
+          <div class='formItems'>
+            <label for='address'>Address</label>
+            <input name='address' id='address' type='text' />
+          </div>
+          <div class='formItems'>
+          <input id='submit' type='submit' value='Save'>
+          </div>
+        </form>
+        <h1>Your Informations</h1>
+
+      </div>
+
+    </div>
+    ";
