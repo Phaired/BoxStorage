baea2aaa4870281fbb05edd978bde4f100059107
Phaired <65019388+Phaired@users.noreply.github.com>
2023-02-07 16:26:43 +0100

    rework database

--- app/src/utils/Database.php
+++ app/src/utils/Database.php
@@ -1,22 +1,46 @@
 <?php
-class Database
-{
-    public $db;
 
-    public function __construct()
-    {
-        $dsn="mysql:dbname=projet;host=database";
-        try
-        {
-            $this->db=new PDO($dsn,"root","password");
-        }
-        catch(PDOException $e)
-        {
-            printf("Echec connexion : %s\n",
-                $e->getMessage());
-            exit();
-        }
+class Database {
+    static private $oPDO = NULL;
+    static private $oInstance = NULL;
 
+    private function __construct() {
+        self::$oPDO = new PDO('mysql:host=database;dbname=projet', 'root', 'password');
+        self::$oPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        self::$oPDO->query("SET NAMES 'utf8'");
     }
 
+    public function __destruct() {
+        self::$oPDO = null ;
+    }
+
+    public function __clone() {
+        throw new Exception('Impossible de cloner une connexion SQL protégée par un singleton');
+    }
+
+    public function __wakeUp( ) {
+        // Vérification de la connexion
+        if(self::$oInstance instanceof self) {
+            throw new MySQLException();
+        }
+        // Correction de la reference
+        self::$oInstance = $this;
+    }
+
+    public function __call($method, $params) {
+        if(self::$oPDO == NULL){
+            self::__construct();
+        }
+
+        return call_user_func_array(array(self::$oPDO, $method), $params);
+    }
+
+    static public function getInstance(){
+        // Verification que l'instance n'a pas déja ete initialisée
+        if(!(self::$oInstance instanceof self)){
+            self::$oInstance = new self();
+        }
+        // Retour de l'instance unique
+        return self::$oInstance;
+    }
 }
