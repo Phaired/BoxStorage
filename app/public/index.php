94e67bf687915f0556ff737d4fca56f13bcc2188
Phaired <65019388+Phaired@users.noreply.github.com>
2023-02-27 14:46:19 +0100

    added router

--- app/public/index.php
+++ app/public/index.php
@@ -1,28 +1,31 @@
 <?php
-$request = $_SERVER['REQUEST_URI'];
-$authorized_pages = ["/", "/login", "/catalog", "/controllers/loginController.php"];
-if(in_array($request, $authorized_pages)) {
-    switch ($request)
-    {
-        case '/' :
-            require '../src/views/home.php';
-            break;
-        case '/login' :
-            require '../src/views/login.php';
-            break;
-        case "/catalog" :
-            require "../src/views/catalog.php";
-            break;
-        case "/controllers/loginController.php":
-            require "../src/controllers/loginController.php";
-            break;
-        default:
-            http_response_code(404);
-            require '../src/views/404.php';
-            break;
-    }
-}
-else {
-    http_response_code(404);
-    require '../src/views/404.php';
+require '../src/router/Router.php';
+
+$router = new Router();
+
+$router->map('GET', '/', function() {
+    require dirname(__DIR__).'/src/views/home.php';
+});
+
+$router->map('GET', '/login', function() {
+    require dirname(__DIR__).'/src/views/login.php';
+});
+
+$router->map('POST','/controllers/loginController.php', function () {
+    require dirname(__DIR__).'/src/controllers/loginController.php';
+});
+
+$router->map('GET', '/catalog', function() {
+    require dirname(__DIR__).'/src/views/catalog.php';
+});
+
+
+
+$match = $router->match();
+
+if( is_array($match) && is_callable( $match['target'] ) ) {
+    call_user_func_array( $match['target'], $match['params'] );
+} else {
+    // no route was matched
+    require dirname(__DIR__).'/src/views/404.php';
 }
