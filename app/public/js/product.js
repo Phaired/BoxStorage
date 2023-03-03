bff48ef5a7e62de3ddb04707c39ba57f30512953
ClementDblt <clementdblt.contact@gmail.com>
2023-03-03 15:09:43 +0100

    product integration

--- app/public/js/product.js
+++ app/public/js/product.js
@@ -16,4 +16,14 @@ window.addEventListener('load', (e) => {
 
 function populateProductDetails(response) {
     console.log(response);
+    document.getElementById("product_image").src = response.imageUrl;
+    document.getElementById("title").textContent = response.title;
+    document.getElementById("price").textContent = response.retailPrice + "$";
+    document.getElementById("product_color").textContent = response.colorway;
+    document.getElementById("product_release").textContent = response.releaseDate;
+    if (response.description != null) {
+        document.getElementById("product_desc").textContent = response.description;
+    } else {
+        document.getElementById("product_desc").style.display = "none";
+    }
 }
--- app/src/views/product.php
+++ app/src/views/product.php
@@ -1,29 +1,30 @@
 <?php
-include "header.php";
+include_once ("header.php");
 ?>
-<link rel="stylesheet" href="css/product_detail.css">
+<link rel="stylesheet" href="../css/product_detail.css">
 <section class="product_section">
     <article class="product">
         <div class="product_img">
             <img id="product_image" src="../img/sample_jordan.png" alt="image product">
         </div>
         <div class="product_body">
-            <h1 class="product_title">Jordan 1</h1>
+            <h1 id="title" class="product_title">Jordan 1</h1>
             <div class="product_size">
                 <select name="size">
                     <option value="">Select size</option>
                 </select>
             </div>
             <div class="product_price">
-                <h2 class="price_value">180$</h2>
+                <h2 id="price" class="price_value">180$</h2>
             </div>
             <div class="product_details">
-                <p id="product_style">Product Style</p><br>
                 <p id="product_color">Product Color</p><br>
                 <p id="product_release">Product release</p><br>
+                <p id="product_desc">Product Style</p><br>
             </div>
             <button id="button_card">Add to Card</button>
         </div>
     </article>
 </section>
+<script type="module" src="../js/product.js"></script>
 <script type="module" src="../js/product.js"></script>
