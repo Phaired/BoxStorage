e98e88857d266746cf6ae2e2b519ba04ff9f1b48
ClementDblt <clementdblt.contact@gmail.com>
2023-03-07 16:11:35 +0100

    add post request in cardController.php

--- app/src/controllers/cardController.php
+++ app/src/controllers/cardController.php
@@ -16,5 +16,15 @@ class cardController {
 
         header("Location: /product/{$id}");
     }
+}
 
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $content = trim(file_get_contents("php://input"));
+    $data = json_decode($content, true);
+
+    $articles = [];
+    foreach ($_SESSION["card"] as $item) {
+        $articles[] = Article::getArticle($item["shoeId"]);
+    }
+    return json_encode($articles);
 }
